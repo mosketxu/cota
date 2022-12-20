c5e93045c43f0a1463ca5c9a4fe03c8911ebf7c3
Alexander Arregui <mosketxu@gmail.com>
2022-12-20 13:56:56 +0100

    Rgister

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,28 +16,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('users')->delete();
-        
-        \DB::table('users')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'name' => 'Alexa',
-                'email' => 'mosketxu@gmail.com',
-                'email_verified_at' => NULL,
-                'password' => '$2y$10$JChGrZsdfFr1Yi8khx4yW.A8SDCW4Wv.WT2cy7dWzdlHxz2TTCSgu',
-                'two_factor_secret' => NULL,
-                'two_factor_recovery_codes' => NULL,
-                'remember_token' => '1ps6I8QhdQnJyaIeLGVawzUAhkbFN4qXFErFVGlZLiDYIkuHR0b63xTIjCjO',
-                'current_team_id' => NULL,
-                'profile_photo_path' => NULL,
-                'created_at' => '2021-02-17 13:43:30',
-                'updated_at' => '2021-03-13 18:00:06',
-            ),
-        ));
-        
-        
+
+        User::create(['name' => 'mosketxu','email' => 'mosketxu@gmail.com','password' => bcrypt('12345678'),
+        ]);
+
+
     }
 }
