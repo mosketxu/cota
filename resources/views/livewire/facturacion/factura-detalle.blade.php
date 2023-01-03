e63440abbd1a2095597cd4970543d6d6e129692c
Alexander Arregui <mosketxu@gmail.com>
2023-01-03 09:15:27 +0100

    Modif

--- resources/views/livewire/facturacion/factura-detalle.blade.php
+++ resources/views/livewire/facturacion/factura-detalle.blade.php
@@ -61,18 +61,17 @@
                 </div>
                 <div class="flex flex-row-reverse w-2/12">
                     @livewire('facturacion.factura-detallenuevo-modal',['factura'=>$factura])
-                    {{-- @livewire('facturacion-conceptos.conceptonuevo-modal',['entidad'=>$entidad]) --}}
                 </div>
             </div>
         </div>
         <div class="py-0.5 ">
             <div class="flex w-full pt-2 pb-0 pl-2 space-x-2 text-xs font-bold text-gray-500 bg-blue-100 rounded-t-md">
-                <div class="flex w-3/12 space-x-1 rounded-l-xl">
-                    <div class="w-2/12"><input type="text" value="Ord" class="w-full py-0.5 text-xs font-bold text-gray-500 truncate bg-blue-100 border-0 rounded-md" disabled/></div>
-                    <div class="w-8/12"><input type="text" value="Agrupación" class="w-full py-0.5 text-xs font-bold text-gray-500 truncate bg-blue-100 border-0 rounded-md" disabled/></div>
+                <div class="flex w-1/12 space-x-1 rounded-l-xl">
+                    {{-- <div class="w-2/12"><input type="text" value="Ord" class="w-full py-0.5 text-xs font-bold text-gray-500 truncate bg-blue-100 border-0 rounded-md" disabled/></div> --}}
+                    <div class="w-10/12"><input type="text" value="Agrup." class="w-full py-0.5 text-xs font-bold text-gray-500 truncate bg-blue-100 border-0 rounded-md" disabled/></div>
                     <div class="w-2/12"></div>
                 </div>
-                <div class="flex w-9/12 space-x-1">
+                <div class="flex w-11/12 space-x-1">
                     <div class="w-16" ><input type="text" value="Ord." class="w-full py-1 text-xs font-bold text-gray-500 truncate bg-blue-100 border-0 rounded-md" disabled /></div>
                     <div class="w-1/12"><input type="text" value="Tipo" class="w-full py-1 text-xs font-bold text-gray-500 truncate bg-blue-100 border-0 rounded-md" disabled /></div>
                     <div class="w-1/12"><input type="text" value="subcta." class="w-full py-1 text-xs font-bold text-gray-500 truncate bg-blue-100 border-0 rounded-md" disabled /></div>
@@ -92,16 +91,16 @@
                 @csrf
                 @method('PUT')
                 <div class="flex w-full pl-2 text-xs text-gray-500 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}">
-                    <div class=" w-3/12 flex space-x-1 pt-0 mt-0 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}" wire:loading.class.delay="opacity-50">
-                        <div class="w-2/12 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}">
+                    <div class=" w-1/12 flex space-x-1 pt-0 mt-0 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}" wire:loading.class.delay="opacity-50">
+                        {{-- <div class="w-2/12 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}">
                             <input type="text" name="orden" value="{{ $detalle->orden }}"
                             class="w-full py-0.5 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }} text-xs font-thin text-gray-500 truncate border-0 rounded-md"/>
-                        </div>
+                        </div> --}}
                         <div class="w-8/12 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}">
                             <input type="text" name="agrup" value="{{ $detalle->concepto }}"
                             class="w-full py-0.5 {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }} text-xs font-thin text-gray-500 truncate border-0 rounded-md"/>
                         </div>
-                        <div class="w-2/12 flex {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}">
+                        <div class="w-4/12 flex {{ $loop->even ? 'bg-yellow-50' : 'bg-green-50' }}">
                             <div class="mt-1">
                             <button type="button" class="text-center btn btn-primary" name="Guardar" onclick="form.submit()">
                                 <x-icon.save />
@@ -119,7 +118,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="w-9/12 pl-2">
+                    <div class="w-11/12 pl-2">
                         @livewire('facturacion.factura-detalle-conceptos',['detalle'=>$detalle,'color'=>$loop->even ? 'yellow' : 'green'],key($detalle->id))
                     </div>
                 </div>
