7506336bcf6da1e44704051e92ad484a8f3a4a0b
Alexander Arregui <mosketxu@gmail.com>
2023-01-12 18:11:35 +0100

    s

--- app/Actions/FacturaCreateAction.php
+++ app/Actions/FacturaCreateAction.php
@@ -13,6 +13,7 @@ class FacturaCreateAction
         $serie= !$factura->serie ? substr($factura->fechafactura->format('Y'), -2) : $factura->serie;
         $factura->metodopago_id= !$factura->metodopago_id ? '1' : $factura->metodopago_id;
 
+
         if (!$factura->numfactura){
             $fac=Facturacion::where('serie', $serie)->max('numfactura') ;
             $fac= $fac ? $fac + 1 : ($serie * 100000 +1) ;
@@ -23,7 +24,7 @@ class FacturaCreateAction
         $factura->ruta=$ruta;
         $caracteresmalos=['.',',',"'"];
 
-        $ent=str_replace($caracteresmalos,"",$factura->entidad->entidad);
+        $ent=str_replace($caracteresmalos,"",$factura->entidad);
         $fichero=(trim('Fra_Suma_'.$factura->serie.'_'.substr ( $fac ,-5 ).'_'.$ent,' ').'.pdf');
         $factura->fichero=substr($fichero, 0, 49);
         $factura->serie=$serie;
